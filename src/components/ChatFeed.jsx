31ca020f19b4b3535e0d10b30a4ad1a4f2ea349d
lkorchnoy <lkorchnoy@yahoo.com>
2021-03-23 09:11:01 -0400

    created message block if my message and structured chatfeed to access title when there is a chat

--- src/components/ChatFeed.jsx
+++ src/components/ChatFeed.jsx
@@ -16,14 +16,35 @@ const ChatFeed = (props) => {
             const lastMessageKey = index === 0 ? null : keys[index-1];
             const isMyMessage = username === message.sender.username;
 
+            return (
+                <div key={`msg_${index}`} style={{ width: "100%" }}>
+                    <div className="message-block">
+                        {
+                            isMyMessage
+                            ? <MyMessage/>
+                            : <UserMessage/>
+                        }
+                    </div>
+                    <div className="read-receipts" style={{ marginRight: isMyMessage ? '18px' : '0px', marginLeft: isMyMessage ? '0px' : '68px'}}>
+                        read-receipts
+                    </div>
+                </div>
+            );
+
         })
 
-        console.log(keys);
+        
     }
       
+    renderMessages()
 
     return (
-        <div>
+        <div className="chat-feed">
+            <div className="chat-title-container">
+                <div className="chat-title">{chat?.title}
+
+                </div>
+            </div>
             ChatFeed
 
         </div>
