4eab5f0ee3676f96cf093c370cca781315403d9c
lkorchnoy <lkorchnoy@yahoo.com>
2021-03-22 22:19:25 -0400

    created index js, in app js put in ID userName and secret from chat engine

--- src/App.js
+++ src/App.js
@@ -1,24 +1,15 @@
-import logo from './logo.svg';
+import { ChatEngine } from 'react-chat-engine';
 import './App.css';
 
-function App() {
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <ChatEngine
+        height="100vh"
+        projectID="a21489bf-cff6-441d-8ac6-f98e1f817552"
+        userName="admin"
+        userSecret="123123"
+
+        />
   );
 }
 
