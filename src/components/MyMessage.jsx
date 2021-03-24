647ca4dd5333d9158e4e09dea6b2e5285e5239d2
lkorchnoy <lkorchnoy@yahoo.com>
2021-03-24 07:13:03 -0400

    passed in prop of a message and defined if message is an image or a text as well as added style to image and text

--- src/components/MyMessage.jsx
+++ src/components/MyMessage.jsx
@@ -1,7 +1,17 @@
-const MyMessage= () => {
+const MyMessage= ({ message }) => {
+    if(message?.attachments?.length > 0) {
+        return (
+            <img 
+            src={message.attachments[0].file}
+            alt="message-attachment"
+            className="message-image"
+            style={{ float: 'right' }}
+            />
+        )
+    }
     return (
-        <div>
-            MyMessage
+        <div className="message" style={{ float: 'right', marginRight: '18px', color: 'beige', background: '3B2A50' }}>
+            {message.text}
         </div>
     );
 }
