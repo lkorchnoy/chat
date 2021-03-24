180c5cbe38bdd475974f95385caaa53481bdd87e
lkorchnoy <lkorchnoy@yahoo.com>
2021-03-24 18:43:34 -0400

    rendered ReadReceipts function and passed in message and isMyMessage props, al  person's avatar comes up as a read receipt

--- src/components/ChatFeed.jsx
+++ src/components/ChatFeed.jsx
@@ -8,6 +8,20 @@ const ChatFeed = (props) => {
 
     const chat = chats && chats[activeChat];
 
+    const renderReadReceipts = (message, isMyMessage) => {
+        return chat.people.map((person, index) => person.last_read === message.id && (
+          <div
+             key={`read_${index}`}
+             className="read-receipt"
+             style={{
+                 float: isMyMessage ? 'right' : 'left',
+                 backgroundImage: `url(${person?.person?.avatar})`
+
+                }}
+             />
+        ))
+    }
+
     const renderMessages = () => {
         const keys = Object.keys(messages);
 
@@ -26,8 +40,8 @@ const ChatFeed = (props) => {
                         }
                     </div>
                     <div className="read-receipts" style={{ marginRight: isMyMessage ? '18px' : '0px', marginLeft: isMyMessage ? '0px' : '68px'}}>
-                        read-receipts
-                    </div>
+                        {renderReadReceipts(message, isMyMessage)}                   
+                        </div>
                 </div>
             );
 
